6738dc3d745c221684766e352d740d92d4121ee3
MARCOS FELIPE MALAQUIAS SERRA <malaquiasserra.marcos@gmail.com>
2025-06-18 16:58:18 -0300

    Update script.js
    voltando ao que era antes

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function exibirDadosNaPagina(dados) {
             if (dados.x265_x272.length > 0) {
                 htmlContent += `<ul>`;
                 dados.x265_x272.forEach((item, index) => {
-                    const displayValue = (item === "" || item === null || item === undefined) ? ;
+                     const displayValue = (item === "" || item === null || item === undefined) ? 'N/A (Dado Ausente)' : item;
                     htmlContent += `<li>${displayValue}</li>`;
                 });
                 htmlContent += `</ul>`;
